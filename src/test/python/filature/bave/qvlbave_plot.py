dfa5bc38ce322bffb99e01cdf2fd7a91766079d7
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-11-29 16:00:41 +0800

    update

--- src/test/python/filature/bave/qvlbave_plot.py
+++ src/test/python/filature/bave/qvlbave_plot.py
@@ -7,12 +7,15 @@ import matplotlib.pyplot as plt
 from filature.bave import qvlbave
 
 plt.figure(figsize=(10.0, 8.0))
+
 bave = qvlbave.QVLBave(1200, 2.8, 1.8 / 2.8, 300.0 / 1200.0)
+bave2 = qvlbave.QVLBave2(1200, 1.8 / 1200, 1 / 1200, 300 / 1200)
 
 pos = np.array([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200])
 
-size = bave.value(pos)
+size1 = bave.value(pos)
+size2 = bave2.value(pos)
 
-print(bave.average(100, 200))
-plt.plot(pos, size, "--")
+plt.plot(pos, size1, "--", "blue")
+plt.plot(pos, size2, "-*", "red")
 plt.show()
